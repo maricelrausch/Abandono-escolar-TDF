d226ad05a2cae3bea17b8e51c6cc1b7b651ff110
Maricel Rausch <maricelmrausch@gmail.com>
2025-05-15 22:30:08 -0300

    Proyecto reorganizado con estructura Cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An{alisis del abandono escolar en Tierra del Fuego',
+    author='Maricel Rausch',
+    license='MIT',
+)
